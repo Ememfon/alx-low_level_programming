1bbe09b3be1cfaff2a6537a4d3b0e129eba27636
Ememfon <ememfoni@gmail.com>
2022-09-14 10:02:01 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
- * main - print putchar, followed by a new line
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 
 int main(void)
+/*
+ * using putchar only for printing
+ *
+ */
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+}
 
-	return (0);
+{
+_putchar('\n');
+}
+return (0);
 }
